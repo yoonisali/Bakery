58e153ece1acf60f817cb9a98061c478a68b668e
Yoonis Ali <yoonisali24@Yooniss-Air.home>
2022-12-09 12:20:31 -0800

    WIP add more UI after lunch

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -11,25 +11,17 @@ namespace Bakery.Program
       Console.WriteLine("Breads: $5 per loaf. Buy 2, get 1 free!");
       Console.WriteLine("Pastries: $2 each or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.");
 
-      Console.Write("How many loaves of bread would you like? ");
+      Console.Write("How many loaves of bread do you wanna buy? ");
       int breadCount = int.Parse(Console.ReadLine());
 
-      Console.WriteLine("How many pastries would you like?");
+      Console.WriteLine("How many pastries do you wanna buy?");
       int pastryCount = int.Parse(Console.ReadLine());
 
       Bread bread = new Bread(breadCount);
       Pastry pastry = new Pastry(pastryCount);
 
       double totalCost = bread.GetCost() + pastry.GetCost();
-      Console.WriteLine($"Your total cost is ${totalCost}.");
-      
-
-
-
-
-
-
-
+      Console.WriteLine($"Your total cost is ${totalCost}. Pay up right now.");
     }
 
   }
